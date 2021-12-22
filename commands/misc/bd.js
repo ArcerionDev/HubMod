fee4567f2b76b9e066cc9cf7e3b5026bc2324204
ArcerionDev <arcerion114@gmail.com>
2021-12-22 10:46:10 -0500

    i forgor

--- commands/misc/bd.js
+++ commands/misc/bd.js
@@ -8,10 +8,11 @@ module.exports = {
     aliases: ['backupdata','bd'],
     categories: [3],
     execute:  function(client,message,args,db,prefix){
+        if (message.author.id != "683792601219989601") return message.reply({ embeds: [new MessageEmbed().setTitle('Invalid').setDescription(`Sorry, you don't have permission to run this command.`)] })
 
         zipdir('./data', { saveTo: './data.zip' }, async () => {
            await message.reply({embeds: [new MessageEmbed().setTitle('Backup').setDescription(`Saved <t:${Math.floor(Date.now() / 1000)}:R>`)],files: ['./data.zip/']})
-            fs.unlinkSync('./data.zip/')
+           try{fs.unlinkSync('./data.zip/')}catch{}
         })
       
     }

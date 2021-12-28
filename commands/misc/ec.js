40300b5faca0d89f5eb2a14c6ef3b771d66e557e
ArcerionDev <arcerion114@gmail.com>
2021-12-28 14:05:15 -0500

    fixing custom poll ending

--- commands/misc/ec.js
+++ commands/misc/ec.js
@@ -16,7 +16,7 @@ module.exports = {
 
             let data = JSON.parse(fs.readFileSync(`./data/custompolls/${args[1]}.json`, 'utf8'))
 
-            if (message.author.tag === data.creator) {
+            if ((message.author.tag === data.creator)||(u.permissions.has('32'))) {
                 let results = []
 
                 Object.keys(data.votes).forEach(v => {
@@ -43,52 +43,27 @@ module.exports = {
 
                     }
 
+                    let ecemb = new MessageEmbed().setTitle(`Results for poll ${data.name}`)
+                    .setDescription(`The poll ${winners.length > 1 ? 'winners are: ' : 'winner is'}\n\n **${winners.join(', ')}**\n\nSubmitted by ${data.creator}.`)
+                    .setImage(`https://quickchart.io/chart?c={type:'pie',data:{labels:${JSON.stringify(data.options)},datasets:[{data:${JSON.stringify(preserved)}}]}}`)
+                    .setFooter(`Poll ID: ${data.id} • Bot developed by Arcerion#7298 🖤`)
 
-                    message.channel.send({ embeds: [new MessageEmbed().setTitle(`Results for poll ${data.name}`).setDescription(`The poll ${winners.length > 1 ? 'winners are: ' : 'winner is'}\n\n **${winners.join(', ')}**\n\nSubmitted by ${data.creator}.`).setImage(`https://quickchart.io/chart?c={type:'pie',data:{labels:${JSON.stringify(data.options)},datasets:[{data:${JSON.stringify(preserved)}}]}}`).setFooter(`Poll ID: ${data.id} • Bot developed by Arcerion#7298 🖤`)] })
+                    try{
 
-                }
+                        message.channel.send({ embeds: [ecemb] })
 
-                fs.unlinkSync(`./data/custompolls/${args[1]}.json`)
+                    }catch{
 
-
-            } else {
-
-                if (!u.permissions.has('32')) return message.reply({ embeds: [new MessageEmbed().setTitle('Invalid').setDescription("You don't have permission to execute this command.")] })
-                let results = []
-
-                Object.keys(data.votes).forEach(v => {
-                    results.push(data.votes[v].length)
-                })
-
-                let preserved = results.slice(0);
-
-                let winners = []
-
-                let wn = results[results.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)]
-
-                if (wn === 0) {
-
-                    message.channel.send({ embeds: [new MessageEmbed().setTitle('Results').setDescription('The vote has ended, but nobody voted :(')] })
-
-                } else {
-
-                    while (results[results.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)] === wn) {
-
-                        winners.push(data.options[results.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)])
-
-                        results[results.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)] = -1
-
+                        ecemb.setImage(undefined)
+                        message.channel.send({ embeds: [ecemb] })
                     }
 
-                    console.log(`https://quickchart.io/chart?c={type:'pie',data:{labels:${JSON.stringify(data.options)},datasets:[{data:${JSON.stringify(preserved)}}]}}`)
-
-                    message.channel.send({ embeds: [new MessageEmbed().setTitle(`Results for poll ${data.name}`).setDescription(`The poll ${winners.length > 1 ? 'winners are: ' : 'winner is'}\n\n **${winners.join(', ')}**\n\nSubmitted by ${data.creator}.`).setImage(`https://quickchart.io/chart?c={type:'pie',data:{labels:${JSON.stringify(data.options)},datasets:[{data:${JSON.stringify(preserved)}}]}}`).setFooter(`Poll ID: ${data.id} • Bot developed by Arcerion#7298 🖤`)] })
-
                 }
 
                 fs.unlinkSync(`./data/custompolls/${args[1]}.json`)
 
-            }
+
+            } 
         })
     }
 }

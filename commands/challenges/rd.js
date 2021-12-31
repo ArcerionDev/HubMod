3fcfe8b70f027070c3beda8aa55866e04045828b
ArcerionDev <arcerion114@gmail.com>
2021-12-31 12:01:23 -0500

    adding random daily command

---
+++ commands/challenges/rd.js
@@ -0,0 +1,47 @@
+module.exports = {
+
+    // put in other things
+    name: "rd",
+    desc: "Get a random daily 30 from the last ~100.",
+    aliases: ['rd','randomdaily'],
+    categories: [0],
+    execute: function(client,message,args,db,prefix){
+
+        message.guild.channels.fetch('484557165340655657').then(c => {
+
+            c.messages.fetch().then(m => {
+            
+            let d = Array.from(Array.from(m)[Math.floor(Math.random()*Array.from(m).length)])[1]
+            
+            
+            const {MessageEmbed} = require('discord.js')
+            let emb = new MessageEmbed()
+            .setTitle(`Daily 30 by ${d.author.tag}`)
+            .setThumbnail(d.author.avatarURL())
+            if(d.content){
+            
+            emb.setDescription(d.content)
+            
+            }
+            
+            
+            message.channel.send({
+            
+            embeds: [
+            
+            emb
+            
+            ],
+            
+            files: [Array.from(d.attachments)[0][1].attachment]
+            
+            })
+            
+            })
+            
+            })
+
+    }
+
+
+}

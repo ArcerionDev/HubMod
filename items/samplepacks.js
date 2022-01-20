ba65e01fcc3ae278676e0e1bcb8eb9c0490613b7
ArcerionDev <arcerion114@gmail.com>
2022-01-20 16:49:57 -0500

    fixing shop issue

--- items/samplepacks.js
+++ items/samplepacks.js
@@ -3,6 +3,7 @@ const prefix = require('../config.json').prefix
 module.exports =  {
     meta: {
       name: "Samplepacks",
+      id: "samplepacks",
       desc: `Purchase community made sample packs! Use ${"`"}${prefix}samplepacks / ${prefix}sp${"`"} for more details.`,
       displayCost: "Varied",
       cost: 0
